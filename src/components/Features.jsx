d73a1611c37600efe70ff2e3afd04479c80da255
Sugacini <sugacinikalaiselvi7@gmail.com>
2025-02-22 12:37:24 +0530

    Theme Changed

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,33 +1,33 @@
 import style from "./Features.module.css";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useState , useRef} from "react";
+import { useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import ChatBot from "./ChatBot";
-import { FaRobot, FaMusic, FaBook, FaQuoteRight, FaUtensils, FaClapperboard, FaGamepad, FaB } from "react-icons/fa6";
+import { FaRobot, FaMusic, FaBook, FaQuoteRight, FaUtensils, FaClapperboard, FaGamepad, FaBookOpen, FaFilePen } from "react-icons/fa6";
 
 let count = 0;
 
 function Features() {
     const setImg = useRef();
 
-    const[emoImg, setEmoImg] = useState("");
+    const [emoImg, setEmoImg] = useState("");
     const location = useLocation();
     const data = location.state;
     const data1 = (JSON.stringify(data.findEmo));
-    const finalEmo = data1.slice(1,data1.length-1);
+    const finalEmo = data1.slice(1, data1.length - 1);
     const finalEmo1 = finalEmo.toUpperCase();
     console.log(JSON.stringify(data.findEmo), finalEmo);
 
     const navigate = useNavigate();
-    
-    if(count == 0){
-        setImg.current = finalEmo+".png";
+
+    if (count == 0) {
+        setImg.current = finalEmo + ".png";
         console.log(setImg.current)
-        setEmoImg(finalEmo+".png");
+        setEmoImg(finalEmo + ".png");
         count++;
     }
-    
+
 
     function quotesHandler() {
         navigate("/quotes");
@@ -41,17 +41,17 @@ function Features() {
         navigate("/story");
     }
 
-    function musicHandler(){
-        navigate("/music", {state: {emo:(finalEmo)}});
+    function musicHandler() {
+        navigate("/music", { state: { emo: (finalEmo) } });
     }
 
-    function chatBotHandler(){
-        navigate("/chatBot",{state: {emo1:(finalEmo)}});
+    function chatBotHandler() {
+        navigate("/chatBot", { state: { emo1: (finalEmo) } });
     }
 
     console.log(setImg.current, emoImg, data1);
     return (
-        
+
         <div className={style.featureOuter}>
             <div className={style.featureContainer}>
 
@@ -60,30 +60,25 @@ function Features() {
                 </div>
 
                 <div className={`${style.item} ${style.feature2}`} onClick={journelHandler}>
-                    <FaBook className={style.icons} />
+                    <FaFilePen className={style.icons} />
                 </div>
 
-                {/* <div className={style.item}>
-                    <i className="fa-solid fa-person-walking"></i>
-                </div> */}
-
                 <div className={`${style.item} ${style.feature3}`} onClick={quotesHandler}>
-                <FaQuoteRight className={style.icons} />
-                    {/* <p className={style.feature}>Quotes</p> */}
+                    <FaQuoteRight className={style.icons} />
                 </div>
 
                 <div className={`${style.item} ${style.feature4}`} onClick={storyHandler}>
-                    <p className={style.feature}>Story</p>
+                    <FaBookOpen className={style.icons} />
                 </div>
 
-                {setImg.current?<div className={style.item1} style={{backgroundImage: `url(fear1.png)`}}>
-                </div>:<div className={style.item1} style={{backgroundImage: `url(${setImg.current})`}}> {finalEmo1} </div>}
+                {setImg.current ? <div className={style.item1} style={{ backgroundImage: `url(fear1.png)` }}>
+                </div> : <div className={style.item1} style={{ backgroundImage: `url(${setImg.current})` }}> {finalEmo1} </div>}
 
                 {/* {setImg.current?<div className={style.item1} style={{backgroundImage: `url(${setImg.current})`}}>
                 </div>:<div className={style.item1} style={{backgroundImage: `url(${setImg.current})`}}> {finalEmo1} </div>} */}
-                
 
-                
+
+
 
                 <div className={`${style.item} ${style.feature5}`}>
                     <FaUtensils className={style.icons} />
